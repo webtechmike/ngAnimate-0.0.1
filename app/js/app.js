d528f3ecc1020d69fedfffcfc983e47bb5b8d374
Mike Bergen <webtechmike@gmail.com>
2014-09-19 00:10:58 -0700

    add first trial - based on egghead lesson

---
+++ app/js/app.js
@@ -0,0 +1,31 @@
+var app = angular.module('app', ['ngAnimate']);
+
+app.controller('AppCtrl', function() {
+    this.isHidden = false;
+    this.fadeIt = function() {
+        this.isHidden = !this.isHidden;
+    }
+});
+
+app.directive('hideMe', function($animate) {
+    return function($scope, element, attrs) {
+        $scope.$watch(attrs.hideMe, function (newval) {
+            if(newval) {
+                $animate.addClass(element, 'fade')
+            } else {
+                $animate.removeClass(element, 'fade')
+            }
+        })
+    }
+});
+
+app.animation(".fade", function() {
+    return {
+        addClass: function(element, className) {
+            TweenMax.to(element, 1, {opacity:0});
+        },
+        removeClass: function(element, className) {
+            TweenMax.to(element, 1, {opacity:1});
+        }
+    }
+})
